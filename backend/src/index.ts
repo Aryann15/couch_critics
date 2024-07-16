35e6c55f3627b4d0bd9cd3864cc70d31c3d26387
Aryann15 <rajlysm15@gmail.com>
2024-07-16 22:59:47 +0530

    signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,36 +1,56 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-app.post('/api/v1/user/signup', (c) => {
-  return c.text('hello from sign up route')
-})
+app.post("/api/v1/user/signup", async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-app.post('/api/v1/user/signin', (c)=>{
-  return c.text('Hello form sign in')
-})
+  try {
+    prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+        name: body.name,
+      },
+    });
+    return c.text("user signed up");
+  } catch (e) {
+    c.status(411);
+    return c.text("user already exsists");
+  }
+});
 
-app.post('/api/v1/post', (c)=> {
-  return c.text ("Hello from posts section")
-})
+app.post("/api/v1/user/signin", (c) => {
+  return c.text("Hello form sign in");
+});
 
-app.put('/api/v1/post', (c) =>{
-  return c.text ('Hello from hono')
-})
+app.post("/api/v1/post", (c) => {
+  return c.text("Hello from posts section");
+});
 
-app.get('/api/v1/post/:id', (c) =>{
-  return c.text ('Hello from hono')
-})
+app.put("/api/v1/post", (c) => {
+  return c.text("Hello from hono");
+});
 
-app.get('/api/v1/post/bulk',(c) =>{
-  return c.text ('Hello from hono')
-})
+app.get("/api/v1/post/:id", (c) => {
+  return c.text("Hello from hono");
+});
 
-export default app
+app.get("/api/v1/post/bulk", (c) => {
+  return c.text("Hello from hono");
+});
 
-
-
+export default app;
