b68f59025d26c84b137e6050911012c83771e0af
Aryann15 <rajlysm15@gmail.com>
2024-07-17 03:51:35 +0530

    middlewares added

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,16 +1,36 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
+import { verify } from "hono/jwt";
 
 export const postRouter = new Hono<{
   Bindings: {
     DATABASE_URL: string;
     SECRET: string;
   };
+  Variables: {
+    userId: string;
+  };
 }>();
+
+postRouter.use("/", async (c, next) => {
+  const authHeader = c.req.header("authorization") || "";
+  const user = await verify(authHeader, c.env.SECRET);
+  if (user) {
+    //@ts-ignore
+    c.set("userId", user.id);
+    next();
+  }else {
+    c.status(401)
+    return c.json ({
+      message: " you are not logged in "
+    })
+  }
+});
 
 postRouter.get("/", async (c) => {
   const body = await c.req.json();
+  const userId = c.get("userId")
   const prisma = new PrismaClient({
     datasourceUrl: c.env?.DATABASE_URL,
   }).$extends(withAccelerate());
@@ -18,7 +38,7 @@ postRouter.get("/", async (c) => {
     data: {
       title: body.title,
       content: body.content,
-      authorId: "2",
+      authorId: userId,
     },
   });
   return c.json({
