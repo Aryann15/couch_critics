43f0e9c48a3e2bff9c8e84cfd03c0674403afa55
Aryann15 <rajlysm15@gmail.com>
2024-07-16 17:31:44 +0530

    routes init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,4 +6,31 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.post('/api/v1/user/signup', (c) => {
+  return c.text('hello from sign up route')
+})
+
+app.post('/api/v1/user/signin', (c)=>{
+  return c.text('Hello form sign in')
+})
+
+app.post('/api/v1/post', (c)=> {
+  return c.text ("Hello from posts section")
+})
+
+app.put('/api/v1/post', (c) =>{
+  return c.text ('Hello from hono')
+})
+
+app.get('/api/v1/post/:id', (c) =>{
+  return c.text ('Hello from hono')
+})
+
+app.get('/api/v1/post/bulk',(c) =>{
+  return c.text ('Hello from hono')
+})
+
 export default app
+
+
+
