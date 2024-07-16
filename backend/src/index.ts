11d26039d49738ef73bc7a1ac5520aebe7dd4e8c
Aryann15 <rajlysm15@gmail.com>
2024-07-17 00:49:42 +0530

    jwt init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { decode, sign, verify } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    SECRET: string;
   };
 }>();
 
@@ -15,43 +17,46 @@ app.get("/", (c) => {
 app.post("/api/v1/user/signup", async (c) => {
   const body = await c.req.json();
   const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
+    datasourceUrl: c.env?.DATABASE_URL,
   }).$extends(withAccelerate());
 
   try {
-    prisma.user.create({
+    const user = await prisma.user.create({
       data: {
         email: body.email,
         password: body.password,
         name: body.name,
       },
     });
-    return c.text("user signed up");
+    console.log("hello")
+    const jwt = await sign({ id: user.id }, c.env.SECRET);
+    return c.json ({jwt})
   } catch (e) {
     c.status(411);
     return c.text("user already exsists");
   }
 });
 
-app.post("/api/v1/user/signin",async (c) => {
-  const body = await c.req.json()
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate());
+app.post('/api/v1/user/signin', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
 
-  try{
-    const user = await prisma.user.findFirst({
-      where: {
-        email : body.email,
-        password: body.password,
-      }
-     
-    })
-    return c.text("welcome back")
-  }catch (e) {
-    return c.text("wrong email/password")
-  }
-});
+	const body = await c.req.json();
+	const user = await prisma.user.findUnique({
+		where: {
+			email: body.email
+		}
+	});
+
+	if (!user) {
+		c.status(403);
+		return c.json({ error: "user not found" });
+	}
+
+	const jwt = await sign({ id: user.id }, c.env.SECRET);
+	return c.json({ jwt });
+})
 
 app.post("/api/v1/post", (c) => {
   return c.text("Hello from posts section");
