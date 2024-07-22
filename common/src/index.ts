aef84291979b212ab1f7004767009bc802abb217
Aryann15 <rajlysm15@gmail.com>
2024-07-22 14:16:58 +0530

    initialising zod variables and types

---
+++ common/src/index.ts
@@ -0,0 +1,31 @@
+import z, { TypeOf } from "zod";
+
+export const signupInput = z.object({
+  username: z.string().email(),
+  password: z.string().min(6),
+  name: z.string().optional(),
+});
+
+
+export const signinInput = z.object({
+  username: z.string().email(),
+  password: z.string().min(6),
+});
+
+
+export const createPostInput = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+
+export const updatePostInput = z.object({
+  title: z.string(),
+  content: z.string(),
+  id: z.string(),
+});
+
+export type SignupInput = z.infer<typeof signupInput>;
+export type SigninInput = z.infer<typeof signinInput>;
+export type createPostInput = z.infer<typeof createPostInput>;
+export type updatePostInput = z.infer<typeof updatePostInput>;
