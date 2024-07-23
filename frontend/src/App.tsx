70b052b0e186f9bab6e7701e1d52e5e3324288e1
Aryann15 <rajlysm15@gmail.com>
2024-07-23 20:16:31 +0530

    basic routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
-function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
 
+function App() {
   return (
     <>
-      <h1>home</h1>
-     
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
