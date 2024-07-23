c7aac96e5f6e700ed3adef630017abbdc56e6e0c
Aryann15 <rajlysm15@gmail.com>
2024-07-23 20:13:50 +0530

    signin page done, reqquests doing fine

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,13 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { LabelInput } from "./Signup";
 import { useState } from "react";
 import { SigninInput } from "aryantech-couchcritics-common";
+import axios from "axios";
+import { backend_url } from "../config";
 
 export const Signin = () => {
-    const [signinInputs, setSigninInputs] = useState<SigninInput>({
-        username : "",
-        password : ""
-    })
+    const navigate = useNavigate()
+  const [signinInputs, setSigninInputs] = useState<SigninInput>({
+    username: "",
+    password: "",
+  });
+
+  async function sendRequest() {
+    try {
+      const response = await axios.post(
+        `${backend_url}/api/v1/user/signin`,
+        signinInputs
+      );
+      const jwt = response.data;
+      localStorage.setItem("token", jwt);
+      navigate('/blogs')
+    } catch (e) {
+      console.log(e);
+     }
+  }
   return (
     <div className="flex justify-center items-center font-mono bg-slate-100 min-h-screen">
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md ">
@@ -15,9 +32,8 @@ export const Signin = () => {
           Sign in to your account
         </div>
         <div className="flex justify-center pb-4 text-base text-slate-700">
-          or{" "}
-          <Link className="underline pl-2" to={"/signup"}>
-            {" "}
+          or
+          <Link className="underline pl-2 font-bold" to={"/signup"}>
             sign up
           </Link>
         </div>
@@ -42,7 +58,7 @@ export const Signin = () => {
           }}
         />
         <div className=" bg-black w-full text-white text text-center rounded-md mt-5 p-2">
-          <button>Submit</button>
+          <button onClick={sendRequest}>Submit</button>
         </div>
       </div>
     </div>
