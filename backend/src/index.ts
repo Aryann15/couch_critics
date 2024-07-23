8960c40407e085b74a2f84a76b3a20a0c7bd89f4
Aryann15 <rajlysm15@gmail.com>
2024-07-23 20:15:06 +0530

    introducing cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,8 @@ import { withAccelerate } from "@prisma/extension-accelerate";
 import { decode, sign, verify } from "hono/jwt";
 import { userRouter } from "./routes/user";
 import { postRouter } from "./routes/posts";
+import { cors } from "hono/cors";
+
 
 const app = new Hono<{
   Bindings: {
@@ -11,7 +13,7 @@ const app = new Hono<{
     SECRET: string;
   };
 }>();
-
+app.use('/*', cors())
 app.route('/api/v1/user',userRouter)
 app.route('/api/v1/post',postRouter)
 export default app;
